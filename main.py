6be73982d4532a6108baba4f375bb907eda39285
tg <opinopee@gmail.com>
2024-07-08 19:50:11 +0530

    rewriting main 👍

--- main.py
+++ main.py
@@ -1,106 +1,51 @@
-from os import system, name
-import keyboard
+class Pos:
+    def __init__(self, pos: tuple[int, int] = (0, 0), board: tuple[int, int] = (9, 9)) -> None:
+        self.pos = pos
+        self.board = board
+    
+    def up(self):
+        self.pos = (self.pos[0], (self.pos[1] - 1) % self.board[1])
+    def down(self):
+        self.pos = (self.pos[0], (self.pos[1] + 1) % self.board[1])
+    def left(self):
+        self.pos = ((self.pos[0] - 1) % self.board[0], self.pos[1])
+    def right(self):
+        self.pos = ((self.pos[0] + 1) % self.board[0], self.pos[1])
 
+    def __repr__(self):
+        return ','.join([str(i) for i in self.pos])
+
+
+from os import system, name
 def clear_screen() -> None:
     system('cls' if name == 'nt' else 'clear')
-    
-def rgb(text: str, clr) -> str:
-    if type(clr) == str:
-        r, g, b = clr[4:-1].replace(',', '').split()
-    else:
-        r, g, b = clr
-    return f"\033[38;2;{r};{g};{b}m{text}\033[0m"
 
-def play(dx: int, dy: int):
-    global pos_x, pos_y
-    board[pos_y][pos_x] = piece
-    pos_x = (pos_x + dx) % board_size[0]
-    pos_y = (pos_y + -dy) % board_size[1]
-    board[pos_y][pos_x] = player
-    display_board()
 
-def display_board() -> None:
-    clear_screen()
-    for row in board:
-        for i in row:
-            if i == player:
-                print(rgb(i, 'rgb(230, 185, 166)'), end=' ')
-            else:
-                print(rgb(i, 'rgb(47, 54, 69)'), end=' ')
-        print()
-
-def display_menu(step: int) -> None:
-    clear_screen()
-    print(rgb("\tWelcome!", 'rgb(54, 186, 152)'))
-    global pos_menu
-    pos_menu = (pos_menu - step) % len(menu)
-    for i in range(len(menu)):
-        if i == pos_menu:
-            s = f'{menu[i] + description[i]}'
-            print(rgb(f'-> {s}', 'rgb(233, 196, 106)'))
-        else:
-            print(rgb(f'{menu[i]}', 'rgb(231, 111, 81)'))
-
-def choice() -> None:
-    if start:
-        pass
-    else:
-        if pos_menu == 0:
-            global start; start = True
-            display_board()
-        elif pos_menu == len(menu) - 1:
-            quit()
-
-def get_key() -> None:
+import keyboard
+def get_key():
     event: keyboard.KeyboardEvent = keyboard.read_event(suppress=True)
-    if event.event_type == keyboard.KEY_UP:
+    if event.event_type == keyboard.KEY_DOWN:
         key: str = event.name.lower()
     else: return
+    
+    if key in ['up', 'w']:
+        pos.up()
+    elif key in ['left', 'a']:
+        pos.left()
+    elif key in ['down', 's']:
+        pos.down()
+    elif key in ['right', 'd']:
+        pos.right()
+        
+    print(pos)
 
-    global start
-    if start:
-        if key in ['esc', 'x', 'q']:
-            start = False
-            global pos_x, pos_y
-            board[pos_y][pos_x] = piece
-            pos_x = pos_y = 0
-            board[pos_y][pos_x] = player
-            display_menu(pos_menu)
-        elif key in ['up', 'w']:
-            play(0, 1)
-        elif key in ['left', 'a']:
-            play(-1, 0)
-        elif key in ['down', 's']:
-            play(0, -1)
-        elif key in ['right', 'd']:
-            play(1, 0)
-        elif key == 'enter':
-            global move
-            move += 1
-            choice()
-    else:
-        if key in ['esc', 'x', 'q']:
-            quit()
-        elif key in ['up', 'w']:
-            display_menu(1)
-        elif key in ['down', 's']:
-            display_menu(-1)
-        elif key == 'enter':
-            choice()
 
-def main() -> None:
-    clear_screen()
-    display_menu(0)
+def main():
     while True:
         get_key()
 
+
 if __name__ == "__main__":
-    game: bool = True; start: bool = False
-    menu: list[str] = ['Start', 'Controls', 'About', 'Exit [esc, x, q]']
-    description: list[str] = [':\n\tenter to start...', ':\n\twasd OR arrows to move\n\tenter to open box\n\tf to flag box', ':\n\tmade by me', '\n\t( ﾉ ﾟｰﾟ)ﾉ']
-    pos_menu: int = 0; pos_x: int = 0; pos_y: int = 0; move: int = 0
-    board_size: tuple[int, int] = (9, 9)
-    piece: str = '📦'; player: str = ' ψ'
-    board: list[list] = [[piece for _ in range(board_size[0])] for _ in range(board_size[1])]
-    board[pos_y][pos_x] = player
+    pos = Pos()
+    clear_screen()
     main()
