828e3f3be4d2aa8e81059e2fdc47b3931f1a2f68
tg <opinopee@gmail.com>
2024-07-09 04:42:24 +0530

    Ain't satisfied with the menu screen colors

--- main.py
+++ main.py
@@ -12,8 +12,28 @@ class Pos:
     def right(self):
         self.pos = ((self.pos[0] + 1) % self.board[0], self.pos[1])
 
+    def __getitem__(self, index: int):
+        return self.pos[index]
+
     def __repr__(self):
         return ','.join([str(i) for i in self.pos])
+
+
+class Color:
+    def __init__(self, value) -> None:
+        self.value = value
+
+    def rgb(self, text: str, clr) -> str:
+        if type(clr) == str:
+            r, g, b = clr[4:-1].replace(',', '').split()
+        else:
+            r, g, b = clr
+        return f"\033[38;2;{r};{g};{b}m{text}\033[0m"
+
+    def header(self) -> str: return self.rgb(self.value, 'rgb(54, 186, 152)')
+    def select(self) -> str: return self.rgb(self.value, 'rgb(233, 196, 106)')
+    def desc(self) -> str: return self.rgb(self.value, 'rgb(130, 150, 140)')
+    def other(self) -> str: return self.rgb(self.value, 'rgb(231, 111, 81)')
 
 
 from os import system, name
@@ -21,11 +41,21 @@ def clear_screen() -> None:
     system('cls' if name == 'nt' else 'clear')
 
 
+def display():
+    if menu_screen:
+        for i, key in enumerate(menu):
+            if pos[1] == i:
+                print(Color(key).select())
+                print(f'  {Color(menu[key]).desc()}')
+            else:
+                print(Color(key).other())
+
+
 import keyboard
 def get_key():
     event: keyboard.KeyboardEvent = keyboard.read_event(suppress=True)
     if event.event_type == keyboard.KEY_DOWN:
-        key: str = event.name.lower()
+        key: str = event.name.lower() # type: ignore
     else: return
     
     if key in ['up', 'w']:
@@ -36,16 +66,27 @@ def get_key():
         pos.down()
     elif key in ['right', 'd']:
         pos.right()
-        
-    print(pos)
+    
+    clear_screen()
+    print(Color("\tWelcome!\n").header())
+    display()
 
 
 def main():
+    print(Color("\tWelcome!\n").header())
+    display()
     while True:
         get_key()
 
 
 if __name__ == "__main__":
-    pos = Pos()
+    menu_screen: bool = True
+    menu: dict[str, str] = {
+        "Start": "enter to start...",
+        "Controls": "controls here",
+        "About": "i am cool",
+        "Exit [esc, q, x]": "( ﾉ ﾟｰﾟ)ﾉ"
+    }
+    pos: Pos = Pos(board=(1, len(menu)))
     clear_screen()
     main()
