b141f7067b232b0079d2aa2d0e41df40d0d7ec8a
tg <opinopee@gmail.com>
2024-07-13 20:02:57 +0530

    setup ing

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="minesweeper-v",
+    name="quantum-minesweeper",
     version="0.1",
     packages=find_packages(),
     include_package_data=True,
@@ -10,7 +10,11 @@ setup(
     ],
     entry_points={
         "console_scripts": [
-            "not_minesweeper=main:main",
+            "quantum-minesweeper=main:main",
         ],
+    },
+    package_data={
+        # Include additional files like ui.py and minesweeper.py
+        '': ['ui.py', 'minesweeper.py'],
     },
 )
