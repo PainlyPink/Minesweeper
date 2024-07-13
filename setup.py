9b154dc4e829028293f873d462a746500a3a166e
tg <opinopee@gmail.com>
2024-07-13 19:21:00 +0530

    setup changes

--- setup.py
+++ setup.py
@@ -1,20 +1,16 @@
-from cx_Freeze import setup, Executable
+from setuptools import setup, find_packages
 
-# Define the main script of your application
-script = "main.py"  # Replace with the name of your main Python file
-
-# Define additional options if necessary
-build_exe_options = {
-    "packages": ["keyboard"],  # List any additional packages your project requires
-    "excludes": ["cx_Freeze", "cx_Logging", "lief", "setuptools", "wheel"],  # List any packages you want to exclude
-    "include_files": ["ui.py", "minesweeper.py"]  # Add ui.py to the included files
-}
-
-# Setup configuration
 setup(
-    name="not-minesweeper",  # Replace with your project name
+    name="minesweeper-v",
     version="0.1",
-    description="A terminal-based Minesweeper game.",  # Description of your project
-    options={"build_exe": build_exe_options},
-    executables=[Executable(script, base=None)],  # base=None is for console applications
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        "keyboard",
+    ],
+    entry_points={
+        "console_scripts": [
+            "not_minesweeper=main:main",
+        ],
+    },
 )
