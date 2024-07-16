b0efe358b5295843c6a2114d8dbbca92ca35c516
tg <opinopee@gmail.com>
2024-07-16 06:36:44 +0530

    ic

--- main.py
+++ main.py
@@ -1,15 +1,15 @@
-from ui import main as ui_main
-from subprocess import run
+# from ui import main as ui_main
+# from subprocess import run
 
-try:
-    import keyboard  # Attempt to import the keyboard module
-except ImportError:
-    print("Module 'keyboard' not installed.")
-    if input("Consent to install module (y/n): ").lower() == 'y':
-        run("pip install keyboard".split())
-    else:
-        print("Aborting execution.")
-        exit()
+# try:
+#     import keyboard  # Attempt to import the keyboard module
+# except ImportError:
+#     print("Module 'keyboard' not installed.")
+#     if input("Consent to install module (y/n): ").lower() == 'y':
+#         run("pip install keyboard".split())
+#     else:
+#         print("Aborting execution.")
+#         exit()
 
-if __name__ == "__main__":
-    ui_main()  # Call the main function from ui.py
+# if __name__ == "__main__":
+#     ui_main()  # Call the main function from ui.py
