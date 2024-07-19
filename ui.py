8f3350f03497eb96ca5634dab4d75a89ae3b3bd5
tg <opinopee@gmail.com>
2024-07-19 19:51:00 +0530

    menu screen progress 1 - tg + specific terminal screen size required ! size check tbi

--- ui.py
+++ ui.py
@@ -1,4 +1,5 @@
 import curses
+import pyfiglet
 
 menu = ('Home', 'Play', 'Scoreboard', 'Exit')
 
@@ -6,6 +7,7 @@ def init_colors():
     """initialize color pairs"""
     curses.init_pair(1, curses.COLOR_YELLOW, curses.COLOR_GREEN)
     curses.init_pair(2, curses.COLOR_CYAN, curses.COLOR_MAGENTA)
+    curses.init_pair(3, curses.COLOR_RED, curses.COLOR_BLACK)
 
 def print_center(stdscr: curses.window, text: str):
     """prints text in center of screen"""
@@ -18,6 +20,10 @@ def print_menu(stdscr: curses.window, select_idx: int):
     """prints menu with highlighted selection"""
     stdscr.clear()
     h, w = stdscr.getmaxyx()
+    
+    header = str(pyfiglet.figlet_format("* minesweeper *"))
+    for i, line in enumerate(header.split('\n')):
+        stdscr.addstr(i + 1, w // 2 - len(line) // 2, line, curses.color_pair(3))
 
     for idx, row in enumerate(menu):
         x = w // 2 - len(row) // 2
@@ -27,26 +33,30 @@ def print_menu(stdscr: curses.window, select_idx: int):
         else:
             stdscr.addstr(y, x, row, curses.color_pair(1))
         
+    # Add navigation instructions
+    stdscr.addstr(h - 1, 0, "WASD or arrow keys to navigate...")
     stdscr.refresh()
 
 def main(stdscr: curses.window):
     """main game loop"""
+    stdscr.nodelay(True)  # Continue even if no key is pressed
+    stdscr.timeout(100)   # Wait 100ms before continuing
     curses.curs_set(0)
     init_colors()
     
     idx = 0
     print_menu(stdscr, idx)
 
-    while 1:
+    while True:
         key = stdscr.getch()
 
-        if key in (curses.KEY_UP, 119):
+        if key in (curses.KEY_UP, 119):  # 119 is 'w'
             idx = (idx - 1) % len(menu)
-        elif key in (curses.KEY_DOWN, 115):
+        elif key in (curses.KEY_DOWN, 115):  # 115 is 's'
             idx = (idx + 1) % len(menu)
-        elif key in (curses.KEY_ENTER, 32, 10, 13):
+        elif key in (curses.KEY_ENTER, 32, 10, 13):  # Space, Enter keys
             print_center(stdscr, f"Entered {menu[idx]} !")
-            if idx == len(menu) - 1:
+            if idx == len(menu) - 1:  # Exit option
                 break
             stdscr.getch()
 
@@ -54,5 +64,6 @@ def main(stdscr: curses.window):
 
 curses.wrapper(main)
 
+# Clear the terminal screen
 from os import system, name
 system('cls' if name == 'nt' else 'clear')
