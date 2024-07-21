b815251a6d36a673d13168bbd1957a73754528c3
a.hfz <adil14nov@gmail.com>
2024-07-21 17:23:21 +0530

    Co-authored-by: ElektrikFire <ElektrikFire@users.noreply.github.com>

--- ui.py
+++ ui.py
@@ -1,7 +1,7 @@
 import curses
 import pyfiglet
 
-menu = ('Home', 'Play', 'Scoreboard', 'Exit')
+menu = ('Play', 'Settings', 'About', 'Exit')
 
 def init_colors():
     """initialize color pairs"""
@@ -16,7 +16,7 @@ def print_center(stdscr: curses.window, text: str):
     stdscr.addstr(y, x, text)
     stdscr.refresh()
 
-def print_menu(stdscr: curses.window, select_idx: int):
+def print_menu(stdscr: curses.window, select_opt: int):
     """prints menu with highlighted selection"""
     stdscr.clear()
     h, w = stdscr.getmaxyx()
@@ -25,10 +25,10 @@ def print_menu(stdscr: curses.window, select_idx: int):
     for i, line in enumerate(header.split('\n')):
         stdscr.addstr(i + 1, w // 2 - len(line) // 2, line, curses.color_pair(3))
 
-    for idx, row in enumerate(menu):
+    for opt, row in enumerate(menu):
         x = w // 2 - len(row) // 2
-        y = h // 2 - len(menu) + idx
-        if idx == select_idx:
+        y = h // 2 - len(menu) + opt
+        if opt == select_opt:
             stdscr.addstr(y, x, row, curses.color_pair(2))
         else:
             stdscr.addstr(y, x, row, curses.color_pair(1))
@@ -37,6 +37,28 @@ def print_menu(stdscr: curses.window, select_idx: int):
     stdscr.addstr(h - 1, 0, "WASD or arrow keys to navigate...")
     stdscr.refresh()
 
+def wait_for_key(stdscr: curses.window): 
+    while stdscr.getch() == -1:
+        continue
+
+def on_enter(stdscr: curses.window):
+    print_center(stdscr, f"Entered {menu[opt]} !")
+    # menu = ('Play', 'Settings', 'About', 'Exit')
+    if opt == 0:
+        stdscr.clear()
+        print_center(stdscr, "in play")
+        wait_for_key(stdscr)
+    elif opt == 1:
+        stdscr.clear()
+        print_center(stdscr, "in Settings")
+        wait_for_key(stdscr)
+    elif opt == 2:
+        stdscr.clear()
+        print_center(stdscr, "in About")
+        wait_for_key(stdscr)
+    elif opt == len(menu) - 1:  # Exit option
+        exit()
+
 def main(stdscr: curses.window):
     """main game loop"""
     stdscr.nodelay(True)  # Continue even if no key is pressed
@@ -44,23 +66,21 @@ def main(stdscr: curses.window):
     curses.curs_set(0)
     init_colors()
     
-    idx = 0
-    print_menu(stdscr, idx)
+    global opt
+    opt = 0
+    print_menu(stdscr, opt)
 
     while True:
         key = stdscr.getch()
-
+        if key == -1: continue
+        
         if key in (curses.KEY_UP, 119):  # 119 is 'w'
-            idx = (idx - 1) % len(menu)
+            opt = (opt - 1) % len(menu)
         elif key in (curses.KEY_DOWN, 115):  # 115 is 's'
-            idx = (idx + 1) % len(menu)
+            opt = (opt + 1) % len(menu)
         elif key in (curses.KEY_ENTER, 32, 10, 13):  # Space, Enter keys
-            print_center(stdscr, f"Entered {menu[idx]} !")
-            if idx == len(menu) - 1:  # Exit option
-                break
-            stdscr.getch()
-
-        print_menu(stdscr, idx)
+            on_enter(stdscr)
+        print_menu(stdscr, opt)
 
 curses.wrapper(main)
 
